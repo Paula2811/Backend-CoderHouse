4fcad88b35f99642232fbb962577817bc62f228e
Paula Yunes <pauliyunes94@gmail.com>
2022-04-05 09:20:23 -0300

    Desafio 4 2.0

--- desafio4/contenedor.js
+++ desafio4/contenedor.js
@@ -1,78 +1,49 @@
-const fs = require ('fs')
+const productos = [
+    {
+        name: 'Jabon Solido Natural', 
+        price: '800', 
+        thumbnail: 'https://i.pinimg.com/originals/12/e7/c5/12e7c5126d992244719b7070788c5b17.jpg',
+        id: 1
+    },
+    {
+        name: 'Shampoo', 
+        price: '800', 
+        thumbnail: 'https://i.pinimg.com/originals/12/e7/c5/12e7c5126d992244719b7070788c5b17.jpg',
+        id: 2
+    },
+    {
+        name: 'Jabon Solido Aromatico', 
+        price: '800', 
+        thumbnail: 'https://i.pinimg.com/originals/12/e7/c5/12e7c5126d992244719b7070788c5b17.jpg',
+        id: 3
+    }
+]
 
-class Contenedor{
-    constructor(archivo){
-        this.archivo = archivo;
-    }
-
-    save(objeto){
-        try {
-            let data = this.getAll()
-            data.push(objeto)
-            for(let i= 0; i< data.length; i++){
-                data[i].id= 1+ i
-            }
-            console.table(data)
-            fs.writeFileSync(this.archivo,JSON.stringify(data))
-            
-        } catch (error) {
-            throw new Error(error,'No se pudo guardar el producto')
-        }
-    }
-
-    getById(id){
-        try{
-            const contenido = this.getAll()
-            return contenido.find(prod => prod.id === id)
-            //console.table(idencontrado)
-        }catch(error){
-            throw new Error(error,"Error al traer el producto")
-        }
-    }
-
-    getAll(){
-        try{
-            let contenido = fs.readFileSync(this.archivo, "utf8");
-            return JSON.parse(contenido)
-        } catch(error){
-            throw new Error(error,"Error al leer el archivo")
-        }
-    }
-
-    deleteById(id){
-        try{
-            const contenido = this.getAll()
-            const deleted = contenido.filter(producto => producto.id !== id)
-            fs.writeFileSync(this.archivo, JSON.stringify(deleted,null,4))
-            console.log('Elemento eliminado')
-            console.table(deleted)
-        }catch(error){
-            throw new Error(error,"Error al borrar el producto")
-        }
-    }
-    
-    async deleteAll(){
-        try{
-            await fs.promises.writeFile(this.archivo, []);
-            console.log("Contenido borrado")
-        } catch(error){
-            throw new Error(error,"Error al borrar todo")
-        }
-    }
-
-    updateProduct(id,productoNuevo){
-        try{
-            const productos = this.getAll()
-            const index = productos.findIndex(prod => prod.id === id)
-            if(productoNuevo.name){productos[index].name= productoNuevo.name}
-            if(productoNuevo.price){productos[index].price=productoNuevo.price}
-            if(productoNuevo.thumbnail){productos[index].thumbnail=productoNuevo.thumbnail}
-            return `El producto ${productoNuevo.name} fue actualizado correctamente`
-        } catch(error){
-            throw new Error(error,"Error al actualizar el producto")
-        }
-        
-    }
+function getAll(){
+    return productos
 }
 
-module.exports = Contenedor;
+function save(objeto){
+    const nuevoProducto= {...objeto, id: productos.length + 1}
+    productos.push(nuevoProducto)
+    return productos
+}
+
+function getById(id){
+    return productos.find(prod => prod.id === id)
+}
+
+function deleteById(id){
+    const deleted = productos.filter(prod => prod.id !== id)
+    console.log("Producto eliminado", deleted)
+}
+
+function updateProduct(id,productoNuevo){
+        const index = productos.findIndex(prod => prod.id === id)
+        if(productoNuevo.name){productos[index].name= productoNuevo.name}
+        if(productoNuevo.price){productos[index].price=productoNuevo.price}
+        if(productoNuevo.thumbnail){productos[index].thumbnail=productoNuevo.thumbnail}
+        return `El producto ${productoNuevo.name} fue actualizado correctamente`
+}
+
+module.exports = {getAll,save,getById,deleteById,updateProduct}
--- desafio4/index.js
+++ desafio4/index.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const Contenedor = require('./contenedor.js')
-const productos = require('./productos.js')
-
-
-
 const app = express();
 const routerProductos = express.Router()
+const {getAll,save,getById,deleteById,updateProduct} = require('./contenedor.js')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/productos',routerProductos)
 app.use(express.static('public'))
 
-const producto = new Contenedor("./produstos.txt")
-
 //1- Obtener todos los productos
 routerProductos.get('/',(req, res) => {
-    let resp = producto.getAll().length !=0
-    ? {productos: producto.getAll()}
+    res.send(getAll())
+    let resp = getAll().length !=0
+    ? {productos: getAll()}
     : {error: 'Producto no encontrado'}
     res.json(resp)
 })
@@ -25,34 +20,32 @@ routerProductos.get('/',(req, res) => {
 //2-Obtener un producto segun la id
 routerProductos.get('/:id',(req, res) => {
     const {id} = req.params
-    let resp = producto.getById(parseInt(id))
-    ? {producto: producto.getById(parseInt(id))}
+    let resp = getById(parseInt(id))
+    ? {producto: getById(parseInt(id))}
     :{error: 'Producto no encontrado'}
     res.json(resp)
 })
 
 //3-Agrega un producto
 routerProductos.post('/',(req, res)=>{
-    let resp =producto.save(req.body)
-    ?{producto: producto.save(req.body)}
+    let resp =save(req.body)
+    ?{producto: save(req.body)}
     :{error: 'Producto no encontrado'}
     res.json(resp)
 })
 
 //4- Actualiza un producto segun id 
 routerProductos.put('/:id',(req, res)=>{
-    const {id} = req.params
+    const {id} = req.params.id
     let productoNuevo = req.body
-    let resp = producto.updateProduct(parseInt(id),productoNuevo)
+    let resp = updateProduct(parseInt(id),productoNuevo)
     res.json(resp)
 })
 
 //5-Elimina un producto segun id
 routerProductos.delete('/:id',(req, res)=>{
-    const {id} = req.params
-    let resp = producto.deleteById(parseInt(id))
-    ? {producto: producto.deleteById(parseInt(id))}
-    :{error: 'Producto no encontrado'}
+    const {id} = req.params.id
+    let resp = deleteById(parseInt(id))
     res.json(resp)
 })
 
@@ -61,3 +54,5 @@ const server = app.listen(PORT,()=>{
     console.log(`Servidor escuchando en el puerto ${server.address().port}`)
 })
 server.on('error',error => console.log(`Error en servidor ${error}`))
+
+module.exports = routerProductos
